1e184d5f294bad4ffaec35866482f8397be71e0f
mathisdrd <mathis1durand@icloud.com>
2025-12-22 12:14:45 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Devoir Maison INFO2b
+DURAND Mathis, 22/12/2025
+ 
+## Question 1
+
+
+## Question 2
+
+
+## Question 3
+
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp 
+ * INFO2A - DM
+ * DURAND Mathis
+ * 22-12-2025
+ */
+
+// Includes STL 
+#include <iostream>
+
+// Includes locaux
+#include "../head/Parser.h"
+
+
+int main() {
+    std::cout << std::endl;
+
+
+    return 0;
+}
